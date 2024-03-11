a749933b14de68da4d03a71ccdabd2dba575b199
palavithakur <tanvithakur046@gmail.com>
2024-03-11 15:09:53 +0530

    first commit

---
+++ src/pages/settings/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SettingsSidebar from "../../component/settings/SettingsSidebar";
+import TermsAndCondition from "../../pages/settings/terms&condition";
+
+function Settings() {
+  return (
+    <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
+      <div className="grid grid-cols-1 xl:grid-cols-12 bg-white dark:bg-darkblack-600 rounded-xl">
+        <SettingsSidebar />
+        <div className="py-8 px-10 col-span-9 tab-content">
+          <TermsAndCondition />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Settings;
